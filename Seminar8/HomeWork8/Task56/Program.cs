9540311ed4d20a8779c36aeab68d58e493253670
sharlottakorde@gmail.com <sharlottakorde@gmail.com>
2022-12-02 14:47:39 +0300

    Task56

---
+++ Seminar8/HomeWork8/Task56/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с 
+наименьшей суммой элементов: 1 строка*/
+
+int[,] Generate2DArray(int lengthRow, int lengthCol, int deviation)
+{
+    int[,] array = new int[lengthRow, lengthCol];
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return array;
+}
